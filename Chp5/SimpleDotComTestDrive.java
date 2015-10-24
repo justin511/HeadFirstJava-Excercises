4c6fe0eea946264d0fef18554508880aa39d21da
Justin Mae <justin511@gmail.com>
2015-10-24 16:43:26 -0400

    Add Chp5 SimpleDotCom & SimpleDotComTestDrive and test file for compiling

---
+++ Chp5/SimpleDotComTestDrive.java
@@ -0,0 +1,15 @@
+package Chp5;
+
+public class SimpleDotComTestDrive {
+	public static void main(String[] args) {
+		SimpleDotCom dot = new SimpleDotCom();
+		int[] locations = {2,3,4};
+		dot.setLocationCells(locations);
+		String guess = "2";
+		String result = dot.checkYourself(guess);	
+	}	
+}
+
+
+
+	
